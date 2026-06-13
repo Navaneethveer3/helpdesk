af348b2f2b61b95e784f02512e6d50fb082e6fe2
Navaneethveer3 <navaneeth42.com@gmail.com>
2026-06-13 15:20:26 +0530

    testing(ETL): tested the ETL pipeline with Unit testcases.

--- src/main/java/com/project/helpdesk/service/DocumentReaderService.java
+++ src/main/java/com/project/helpdesk/service/DocumentReaderService.java
@@ -1,23 +1,31 @@
 package com.project.helpdesk.service;
 
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
 
+@Service
 public class DocumentReaderService {
 
 	@Value("classpath:Spring Boot.pdf")
 	private Resource resource;
 	
+	@Autowired
+	private VectorStore vectorStore;
 
 	public List<Document> loadDocument(){
 		TikaDocumentReader reader = new TikaDocumentReader(this.resource);
 		TokenTextSplitter splitter = new TokenTextSplitter();
-		return splitter.apply(reader.read());
+		List<Document> transformed = splitter.apply(reader.read());
+		vectorStore.write(transformed);
+		return transformed;
 	}
 }
---
+++ src/test/java/com/project/helpdesk/DocumentReaderServiceTests.java
@@ -0,0 +1,38 @@
+package com.project.helpdesk;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.document.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.project.helpdesk.service.AiService;
+import com.project.helpdesk.service.DocumentReaderService;
+
+@SpringBootTest
+class DocumentReaderServiceTests {
+
+	@Autowired
+	private DocumentReaderService docService;
+
+	@Autowired
+	private AiService aiService;
+	
+//	@Test
+//	void checkLoadDocument() {
+//		List<Document> docs = this.docService.loadDocument();
+//		assertNotNull(docs);
+//		docs.stream().forEach(item->System.out.println(item));
+//	}
+	
+	@Test
+	void checkRag() {
+		String prompt = "Explain about API Security and OAuth2?";
+		String response = this.aiService.getResponseFromAssistant(prompt, "tester");
+		assertNotNull(response);
+		System.out.println(response);
+	}
+}
