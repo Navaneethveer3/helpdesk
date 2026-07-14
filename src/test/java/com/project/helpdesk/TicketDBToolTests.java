9d5b441b6e09543b28a0ca11c485400e9cc657fe
Navaneethveer3 <navaneeth42.com@gmail.com>
2026-07-14 14:15:34 +0530

    testing(chatbot) : tested the tool usage by the chatbot manually using POSTMAN and frontend.

---
+++ src/test/java/com/project/helpdesk/TicketDBToolTests.java
@@ -0,0 +1,14 @@
+package com.project.helpdesk;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TicketDBToolTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
